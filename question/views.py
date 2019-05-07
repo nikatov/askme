4204df8f8852ef323cc78c0cca63eac0974e9ca7
Vladislav Nikatov <vlad.nikatov@mail.ru>
2019-05-08 02:54:59 +0300

    реализован запуск web-сервера + рефакторинг html + css

--- question/views.py
+++ question/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def index(request):
+    questions_list = [
+        {"name": "my best questiion", "id": 1},
+        {"name": "my best questiion2", "id": 2},
+    ]
+    return render(request, "question/base.html", {
+        "questions": questions_list,
+    })
